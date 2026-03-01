b74535d72a6abfe2583361ad1db8c76fc9579eeb
Илья Самович <samovichilias19life@gmail.com>
2026-03-01 21:50:16 +0300

    connect tailwindcss

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,12 +1,9 @@
-import { useState, useEffect } from "react";
-
 interface Todo {
   id: number;
   text: string;
   done: boolean;
 }
 
-// Типизация window для electronAPI
 declare global {
   interface Window {
     api: {
@@ -17,69 +14,16 @@ declare global {
 }
 
 export default function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const [input, setInput] = useState("");
-  const [saved, setSaved] = useState(false);
-
-  useEffect(() => {
-    window.api.loadTodos().then(setTodos);
-  }, []);
-
-  const addTodo = () => {
-    if (!input.trim()) return;
-    const newTodo: Todo = { id: Date.now(), text: input.trim(), done: false };
-    setTodos((prev) => [...prev, newTodo]);
-    setInput("");
-  };
-
-  const toggleTodo = (id: number) => {
-    setTodos((prev) =>
-      prev.map((t) => (t.id === id ? { ...t, done: !t.done } : t)),
-    );
-  };
-
-  const deleteTodo = (id: number) => {
-    setTodos((prev) => prev.filter((t) => t.id !== id));
-  };
-
-  const saveTodos = async () => {
-    await window.api.saveTodos(todos);
-    setSaved(true);
-    setTimeout(() => setSaved(false), 2000);
-  };
+  // const saveTodos = async () => {
+  //   await window.api.saveTodos(todos);
+  //   setSaved(true);
+  //   setTimeout(() => setSaved(false), 2000);
+  // };
 
   return (
-    <div className="container">
-      <h1>📝 Мои задачи</h1>
+    <div className="h-full w-full border border-solid border-amber-50 mx-auto mt-10 p-8 bg-red-300 rounded-2xl">
+      <h1 className="bg-red-500 mb-6 text-[1.8rem] font-semibold">📝 Мои задачи</h1>
 
-      <div className="input-row">
-        <input
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          onKeyDown={(e) => e.key === "Enter" && addTodo()}
-          placeholder="Новая задача..."
-        />
-        <button onClick={addTodo}>Добавить</button>
-      </div>
-
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id} className={todo.done ? "done" : ""}>
-            <span onClick={() => toggleTodo(todo.id)}>{todo.text}</span>
-            <button className="del" onClick={() => deleteTodo(todo.id)}>
-              ✕
-            </button>
-          </li>
-        ))}
-      </ul>
-
-      {todos.length > 0 && (
-        <button className="save-btn" onClick={saveTodos}>
-          {saved ? "✅ Сохранено!" : "💾 Сохранить в файл"}
-        </button>
-      )}
-
-      <p className="hint">Файл: %APPDATA%\electron-todo\todos.json</p>
     </div>
-  );
+  )
 }
